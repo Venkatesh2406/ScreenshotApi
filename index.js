a70de17e47041365e7953dcf7677a8132d599285
Venkatesh2406 <109074201+Venkatesh2406@users.noreply.github.com>
2023-01-01 20:23:59 +0530

    Third Commit with added updates

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
+const {body,query,validationResult}=require('express-validator');
+const nodemailer=require("nodemailer");
+const fs = require('fs')
 require('dotenv').config({path:"D:/Screenshot/.env"})
 app = express();
-bodyParser = require('body-parser')
-console.log(process.env.PORT_NUMBER)
+
+bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); 
-app.use(express.static("D:/Screenshot"+'/public'))
-app.post('/write',(async (req,res)=> {
+app.use(express.static("/Screenshot"))
+
+app.post('/screenshot',
+body("url").exists().isLength({min:1}).withMessage("Check url"),
+body("username").exists().isLength({min:1}).withMessage("Check username"),
+body("password").exists().isLength({min:1}).withMessage("Check Password"),
+body("personnumber").exists().isLength({min:1}).withMessage("Check personnumber")
+,(async (req,res)=> {
+
+  const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+    // Build your resulting errors however you want! String, object, whatever - it works!
+    return `${location}[${param}]: ${msg}`;
+  };
+        const errors=validationResult(req).formatWith(errorFormatter)
+        if(!errors.isEmpty()){
+          return res.status(422).json({errors:errors.array()})
+        }
         let payperiod=""
         let startdate=""
         let enddate=""
-        let headerless=true
+        let headless=true
         if(req.body.Payperiod!=null){
           payperiod=req.body.Payperiod
         }
         if(req.body.startdate!=null){
-          startdate=req.body.startdate.replace("/","")
+          startdate=req.body.startdate.replaceAll("/","")
         }
         if(req.body.enddate!=null){
-          enddate=req.body.enddate.replace("/","")
+          enddate=req.body.enddate.replaceAll("/","")
         }
         if(req.body.headless!=null){
           headerless=req.body.headless
         }
         console.log(payperiod)
+    
         const browser = await puppeteer.launch({
         args: ['--no-sandbox'],
         headless:req.body.headless
       });
+      const url=req.body.url+"/timekeeping#/timecard"
+    
+      try{
         const page = await browser.newPage();
-        await page.goto(req.body.url,{waitUntill:'networkidle2'});
+        await page.goto(url,{waitUntill:'networkidle2'});
         await page.setViewport({width:1200, height:1000});
         await page.waitForSelector('#idToken1')
         await page.waitForSelector('#wrapper')
@@ -46,7 +68,10 @@ app.post('/write',(async (req,res)=> {
         await page.type('#combo_searchbox',req.body.personnumber);
         await page.waitForTimeout(3000)
         await page.click("#combo_li0 > a")
-        await page.waitForTimeout(2000)
+        await page.waitForTimeout(3000)
+        await page.waitForSelector("#employeeInformationWrapper > div.emp-nav-list > div.emp-nav-id")
+        const element=await page.$('#employeeInformationWrapper > div.emp-nav-list > div.emp-nav-id')
+        const personnumber=await page.evaluate(el => el.textContent, element)
         if(payperiod===0 && startdate===null && enddate===null){
           await page.click('#_timeFrames_dropdownMenu > span.timeframe.btn-link > button')
           await page.waitForTimeout(3000)
@@ -85,21 +110,250 @@ app.post('/write',(async (req,res)=> {
           await page.click('#_timeFrames_expandable > div.timeframe-period > div.mid-content > div > ul > li:nth-child(3)')
         }
       }
+        await page.waitForSelector("#_timeFrame")
+        const element1=await page.$('#_timeFrame')
+        const spanname=await page.evaluate(el => el.textContent, element1)
+        const span=spanname.replaceAll("/","-")
+        const filename=personnumber+" "+req.body.Description+" "+span+".png"
+        console.log(filename)
         await page.waitForTimeout(3000)
         await page.waitForSelector("#tile_tile_7 > span")
         await page.click('#tile_tile_7 > span')
         await page.waitForTimeout(4000)
-        await page.screenshot({path:"./pic.png"});
+        await page.screenshot({path:"./Screenshots/"+filename});
         await page.waitForTimeout(3000)
         await page.click('#hamburgerMenu > span')
         await page.waitForTimeout(3000)
         await page.click('#profileSignOut_button')
         await browser.close(); // <-- close browser after everything is done*/
-      res.download('pic.png',{ root: '.' })
+        if(req.body.email!=null || req.body.email!=""){
+            transporter=nodemailer.createTransport({
+            service:"gmail",
+            auth:{
+            user:"mailtesting5656@gmail.com",
+            pass:"ecguicfgiwxoyqwu"
+            },
+            tls:{
+            rejectUnauthorized:false,
+            }
+            });
+            
+            let mailOptions={
+            from:"mailtesting5656@gmail.com",
+            to:req.body.email,
+            subject:"Testing Result of "+req.body.Description,
+            attachments:[
+            {filename:filename || "pic.png",path:"./Screenshots/"+filename || "./Screenshots/pic.png"}
+            ]
+            };
+            transporter.sendMail(mailOptions,function(err,success){
+            if(err){
+               console.log(err)
+            }else{
+               console.log("Email is Sent")
+            }
+            })
+        }
+      res.status(200).download(filename,{ root: './Screenshots' })
+    }catch(err){
+      await browser.close()
+      res.status(500).send(err)
+    }
     }))
 
+    app.get('/screenshot',
+    query("url").exists().isLength({min:1}).withMessage('Check URL'),
+    query("username").exists().isLength({min:1}).withMessage('Check Username'),
+    query("password").exists().isLength({min:1}).withMessage('Check Password'),
+    query("personnumber").exists().isLength({min:1}).withMessage('Check Person Number'),
+
     
-    app.listen(process.env.PORT_NUMBER || 8080, () => {
+    (async (req,res)=> {
+
+      const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+        // Build your resulting errors however you want! String, object, whatever - it works!
+        return `${location}[${param}]: ${msg}`;
+      };
+            const errors=validationResult(req).formatWith(errorFormatter)
+            if(!errors.isEmpty()){
+              return res.status(422).json({errors:errors.array()})
+            }
+      
+      let payperiod=""
+      let startdate=""
+      let enddate=""
+      let headerless=true
+      if(req.query.Payperiod!=null){
+        payperiod=parseInt(req.query.Payperiod)
+      }
+      if(req.query.startdate!=null){
+        startdate=req.query.startdate.replaceAll("/","")
+      }
+      if(req.query.enddate!=null){
+        enddate=req.query.enddate.replaceAll("/","")
+      }
+      if(req.query.headless!=null){
+        headerless=req.query.headless
+      }
+      console.log(payperiod)
+      const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+      headless:false
+    });
+    if(isNaN(payperiod)){
+      payperiod=""
+    }
+    const url=req.query.url+"/timekeeping#/timecard"
+    try{
+      const page = await browser.newPage();
+      await page.goto(url,{waitUntill:'networkidle2'});
+      await page.setViewport({width:1200, height:1000});
+      await page.waitForSelector('#idToken1')
+      await page.waitForSelector('#wrapper')
+      await page.type('#idToken1',req.query.username,{delay:100})
+      await page.type('#idToken2',req.query.password,{delay:100})
+      await page.waitForTimeout(1000)
+      await page.click('#loginButton_0')
+      await page.waitForNetworkIdle({idleTime:3000})
+      await page.waitForSelector('#combo_toggleBtn > i')
+      await page.click('#combo_toggleBtn > i')
+      await page.waitForTimeout(3000)
+      await page.click('#combo_searchbox')
+      await page.type('#combo_searchbox',req.query.personnumber);
+      await page.waitForTimeout(3000)
+      await page.click("#combo_li0 > a")
+      await page.waitForTimeout(3000)
+      await page.waitForSelector("#employeeInformationWrapper > div.emp-nav-list > div.emp-nav-id")
+        const element=await page.$('#employeeInformationWrapper > div.emp-nav-list > div.emp-nav-id')
+        const personnumber=await page.evaluate(el => el.textContent, element)
+        if(payperiod===0 && startdate===null && enddate===null){
+          await page.click('#_timeFrames_dropdownMenu > span.timeframe.btn-link > button')
+          await page.waitForTimeout(3000)
+          await page.click('#_timeFrames_expandable > div.timeframe-period > div.mid-content > div > ul > li:nth-child(1) > span')
+          await page.waitForSelector('#tile_tile_7 > span')
+        }
+        if(payperiod===2 && startdate===null && enddate===null){
+          await page.click('#_timeFrames_dropdownMenu > span.timeframe.btn-link > button')
+          await page.waitForTimeout(3000)
+          await page.click('#_timeFrames_expandable > div.timeframe-period > div.mid-content > div > ul > li:nth-child(3)')
+        }
+        if(startdate && enddate && payperiod===""){
+          await page.click('#_timeFrames_dropdownMenu > span.timeframe.btn-link > button')
+          await page.waitForTimeout(3000)
+          await page.click('#_timeFrames_expandable > div.timeframe-period > div.fixed-footer > div > button')
+          await page.click('#startDateTimeInput')
+          await page.waitForTimeout(1000)
+          await page.type('#startDateTimeInput',startdate)
+          await page.click('#endDateTimeInput')
+          await page.waitForTimeout(1000)
+          await page.type('#endDateTimeInput',enddate)
+          await page.waitForTimeout(2000)
+          await page.click('#tfsApplyButton > span')
+          await page.waitForNetworkIdle({idleTime:5000})
+      }
+      if(payperiod!=null && startdate!=null && enddate!=null){
+        if(payperiod===0){
+          await page.click('#_timeFrames_dropdownMenu > span.timeframe.btn-link > button')
+          await page.waitForTimeout(3000)
+          await page.click('#_timeFrames_expandable > div.timeframe-period > div.mid-content > div > ul > li:nth-child(1) > span')
+          //await page.waitForSelector('#tile_tile_7 > span')
+        }
+        if(payperiod===2){
+          await page.click('#_timeFrames_dropdownMenu > span.timeframe.btn-link > button')
+          await page.waitForTimeout(3000)
+          await page.click('#_timeFrames_expandable > div.timeframe-period > div.mid-content > div > ul > li:nth-child(3)')
+        }
+      }
+        await page.waitForSelector("#_timeFrame")
+        const element1=await page.$('#_timeFrame')
+        const spanname=await page.evaluate(el => el.textContent, element1)
+        const span=spanname.replaceAll("/","-")
+        const filename=personnumber+" "+span+".png"
+        console.log(filename)
+        await page.waitForTimeout(3000)
+        await page.waitForSelector("#tile_tile_7 > span")
+        await page.click('#tile_tile_7 > span')
+        await page.waitForTimeout(4000)
+        await page.screenshot({path:"./Screenshots/"+filename});
+        await page.waitForTimeout(3000)
+        await page.click('#hamburgerMenu > span')
+        await page.waitForTimeout(3000)
+        await page.click('#profileSignOut_button')
+        await browser.close(); // <-- close browser after everything is done*/
+      res.status(200).download(filename,{ root: './Screenshots' })
+    }catch(err){
+      await browser.close()
+      res.status(500).send(err)
+    }
+    }))
+
+    app.post('/send', (req, res) => {
+    transporter=nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+    user:"mailtesting5656@gmail.com",
+    pass:"ecguicfgiwxoyqwu"
+    },
+    tls:{
+    rejectUnauthorized:false,
+    }
+    });
+    attachmentlist={}
+    
+    let mailOptions={
+    from:"mailtesting5656@gmail.com",
+    to:req.body.email,
+    subject:"Testing Result",
+    attachments:[
+    {filename:req.body.filename,path:"./Screenshots/"+req.body.filename}
+    ]
+    };
+    
+    transporter.sendMail(mailOptions,function(err,success){
+    if(err){
+    console.log(err)
+    res.status(500).json("Something went wrong")
+    }else{
+      res.status(200).json("Email is Sent")
+    console.log("Email is Sent")
+    }
+    })
+  })
+
+  app.get('/filelist',(req,res)=>{
+    let filelist=""
+    try{
+
+    fs.readdirSync("/Screenshot/Screenshots").forEach(file => {
+      filelist+=file+"\n"
+      
+    });
+    res.status(200).send(filelist)
+  }catch{
+    res.status(500).json(err)
+  }
+
+  })
+
+  app.get('/filelist/:id',(req,res)=>{
+    let filelist=""
+    //console.log(req.params.id)
+    try{
+
+    fs.readdirSync("/Screenshot/Screenshots").filter(file => file.startsWith(req.params.id)).forEach(file => {
+      filelist+=file+"\n"
+      
+    });
+    //console.log(filelist)
+    res.status(200).send(filelist)
+  }catch{
+    res.status(500).json(err)
+  }
+
+  })
+
+
+  app.listen(process.env.PORT_NUMBER || 8080, () => {
         console.log('ResponsesToFile App is listening now! Send them requests my way!');
         console.log(`Data is being stored at location:`);
       });
